8d9ac4cd19165c0a49ba9f767896f62561e11432
onaikepo <82336362+onaikepo@users.noreply.github.com>
2022-10-10 20:00:42 +0100

    commit for updated index js

--- pages/api/items.js
+++ pages/api/items.js
@@ -10,3 +10,30 @@ export default async (_req, res) => {
         res.status(500).json({ msg: "Something went wrong! 😕" });
     }
 };
+
+function Items({ minfiedItems }) {
+    return (
+        { minfiedItems }
+        < br >
+    )
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries.
+export async function getStaticProps() {
+    // Call an external API endpoint to get posts.
+    // You can use any data fetching library
+    const res = await fetch('https://.../posts')
+    const posts = await res.json()
+
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+        props: {
+            posts,
+        },
+    }
+}
+
+export default Blog
--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
         </div>
         <br></br>
         <div className="container">
-          <form onSubmit={submitSearch} action="airtable">
+          <form onSubmit={submitSearch} action="Airtable">
             <label htmlFor="Search Query">Enter Your Search Query Here:</label>
             <input type="text" name="search" id="search-query" />
             <button type="submit">Search</button>
