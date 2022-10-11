bc3cba7d59bdb277e40a900485556318ad12a7f9
onaikepo <82336362+onaikepo@users.noreply.github.com>
2022-10-11 10:02:22 +0100

    fix for itemsjs

--- pages/api/items.js
+++ pages/api/items.js
@@ -11,29 +11,3 @@ export default async (_req, res) => {
     }
 };
 
-function Items({ minfiedItems }) {
-    return (
-        { minfiedItems }
-        < br >
-    )
-}
-
-// This function gets called at build time on server-side.
-// It won't be called on client-side, so you can even do
-// direct database queries.
-export async function getStaticProps() {
-    // Call an external API endpoint to get posts.
-    // You can use any data fetching library
-    const res = await fetch('https://.../posts')
-    const posts = await res.json()
-
-    // By returning { props: { posts } }, the Blog component
-    // will receive `posts` as a prop at build time
-    return {
-        props: {
-            posts,
-        },
-    }
-}
-
-export default Blog
