680602b156780947cf39b23b60e3a49b79f70e4d
onaikepo <82336362+onaikepo@users.noreply.github.com>
2022-10-11 10:47:13 +0100

    updated API logic

--- utils/Airtable.js
+++ utils/Airtable.js
@@ -1,6 +1,33 @@
-const Airtable = require("/node_modules/airtable");
+const Airtable = require("airtable");
 
-// Authenticate
+
+var base = new Airtable({ apiKey: process.env.Airtable_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
+
+// const base = Airtable.base();
+
+base('Table 1').select({
+    //Only want to return data from ID, Website Name and Description
+    fields: ["ID", "Website Name", "Website Description"],
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 3,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function (record) {
+        console.log('Retrieved', record.get('ID'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
+
+/*// Authenticate
 Airtable.configure({
     apiKey: process.env.AIRTABLE_API_KEY,
 });
@@ -9,22 +36,4 @@ Airtable.configure({
 const base = Airtable.base(process.env.AIRTABLE_BASE_ID);
 
 // Reference a table
-const table = base(process.env.AIRTABLE_TABLE_NAME);
-
-export { table };
-
-const minifyItems = (records) =>
-    records.map((record) => getMinifiedItem(record));
-
-// to make record meaningful.
-const getMinifiedItem = (record) => {
-    if (!record.fields.websiteName) {
-        record.fields.websiteName = false;
-    }
-    return {
-        id: record.id,
-        fields: record.fields,
-    };
-};
-
-export { table, minifyItems, getMinifiedItem };
+const table = base(process.env.AIRTABLE_TABLE_NAME);*/
