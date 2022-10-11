dee2cab59a1fc235be123bc353b2f6ef23d0838e
onaikepo <82336362+onaikepo@users.noreply.github.com>
2022-10-11 15:33:32 +0100

    change to base

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const submitSearch = async (event) => {
   }*/
   //var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base();
   var Airtable = require('airtable');
-  var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_TABLE_ID);
+  var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_TABLE_NAME);
   base('Table 1').select({
     view: 'Grid view'
   }).firstPage(function (err, records) {
