40f04e771b2cc8916ccf0e859d70bca68c4c724d
onaikepo <82336362+onaikepo@users.noreply.github.com>
2022-10-11 12:39:50 +0100

    changes to logic api

--- utils/Airtable.js
+++ utils/Airtable.js
@@ -11,11 +11,11 @@ export default function AirtableFetch() {
     // const base = Airtable.base();
 
     base('Table 1').select({
-        //Only want to return data from ID, Website Name and Description
-        fields: ["ID", "Website Name", "Website Description"],
+        //Only want to return data from ID
+        fields: ["ID"],
         // Selecting the first 3 records in Grid view:
-        maxRecords: 3,
-        view: "Grid view"
+        maxRecords: 1,
+        //view: "Grid view"
     }).eachPage(function page(records, fetchNextPage) {
         // This function (`page`) will get called for each page of records.
 
